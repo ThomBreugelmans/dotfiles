157bb5830bf81532a83bc152c541762919f05da7
LECT3R <lecter@onionmail.org>
2023-07-26 11:00:00 +0200

    Added new nvim and tmux configs

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -1,4 +1,3 @@
-
 vim.opt.guicursor = ""
 
 vim.opt.nu = true
@@ -15,8 +14,6 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -30,5 +27,3 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "100"
-
-
