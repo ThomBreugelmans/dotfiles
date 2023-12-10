9d7f275b7210f0be23d9df4f9325a2c535fbf36f
Thom Breugelmans <thombreugelmans@outlook.com>
2023-12-10 22:00:22 +0100

    changed install to use cp instead of ln
    hardlinks are impossible if the dotfiles are stored in another partition and softlinks result in programs not always being able to read the file contents

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@ HOME_DIR=$HOME
 # LINKING WALLPAPERS #
 ######################
 echo "[+] Linking wallpapers to /usr/share/backgrounds"
-ln -s ./backgrounds /usr/share/
+sudo cp ./backgrounds /usr/share/
 
 ###################
 # SETTING UP SDDM #
 ###################
 echo "[+] Setting up SDDM config"
-rm /usr/share/sddm/themes/sugar-candy/theme.conf /usr/lib/sddm/sddm.conf.d/default.conf > /dev/null
-ln -s ./.config/sddm/theme.conf /usr/share/sddm/themes/sugar-candy/theme.conf
-ln -s ./.config/sddm/default.conf /usr/lib/sddm/sddm.conf.d/default.conf
+sudo rm /usr/share/sddm/themes/sugar-candy/theme.conf /usr/lib/sddm/sddm.conf.d/default.conf > /dev/null
+sudo cp ./.config/sddm/theme.conf /usr/share/sddm/themes/sugar-candy/theme.conf
+sudo cp ./.config/sddm/default.conf /usr/lib/sddm/sddm.conf.d/default.conf
 
 ##################
 # SETTING UP ZSH #
@@ -25,24 +25,24 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo "[+] Linking config file for ZSH (.zshrc)"
-sudo ln -s ./.zshrc $HOME_DIR/.zshrc
+cp ./.zshrc $HOME_DIR/.zshrc
 
 ####################
 # SETTING UP EMACS #
 ####################
 echo "[+] Linking config file for emacs (init.el)"
 # setting up
-mkdir ~/.emacs.d/ > /dev/null
-sudo ln -s ./.emacs.d/init.el $HOME_DIR/.emacs.d/init.el
+mkdir $HOME_DIR/.emacs.d/ > /dev/null
+sudo cp ./.emacs.d/init.el $HOME_DIR/.emacs.d/init.el
 
 ####################
 # SETTING UP KITTY #
 ####################
 echo "[+] Linking config file for kitty (kitty.conf)"
-sudo ln -s ./.config/kitty $HOME_DIR/.config/
+sudo cp ./.config/kitty $HOME_DIR/.config/
 
 ####################
 # SETTING UP QTILE #
 ####################
 echo "[+] Linking config file for qtile (config.py)"
-sudo ln -s ./.config/qtile $HOME_DIR/.config/
+sudo cp ./.config/qtile $HOME_DIR/.config/
